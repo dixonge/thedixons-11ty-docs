715204260f5aa022b22c2b0ad9c4f7fba4dd518e
Glenn Dixon <donblanco@posteo.net>
2020-03-31 13:34:47 -0600

    switch from picture to img

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -1,3 +1,4 @@
+/*
 module.exports = (url, alt = "Missing alt text") => {
     return `<picture>
     <source srcset="/img/${url}?nf_resize=fit&w=320" media="(max-width: 320px)">
@@ -7,4 +8,12 @@ module.exports = (url, alt = "Missing alt text") => {
     <source srcset="/img/${url}?nf_resize=fit&w=1024" media="(max-width: 1024px)">
     <img src="/img/${url}?nf_resize=fit&w=500" alt="${alt}" />
   </picture>`;
+  };
+*/
+  
+module.exports = (url, alt = "Missing alt text") => {
+    return `<img src="/img/${url}?nf_resize=fit&w=320" 
+    srcset="/img/${url}?nf_resize=fit&w=320 320w, /img/${url}?nf_resize=fit&w=375 375w, /img/${url}?nf_resize=fit&w=414 414w, /img/${url}?nf_resize=fit&w=768 768w, /img/${url}?nf_resize=fit&w=1024 1024w"
+    sizes=""
+    alt="${alt}">`;
   };
