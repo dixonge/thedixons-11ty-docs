1d22ccfdfe3a94beb1cd6298fea2d6e7eb154a54
Glenn Dixon <donblanco@posteo.net>
2020-03-31 12:58:49 -0600

    changed all min-width to max-

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -1,10 +1,10 @@
 module.exports = (url, alt = "Missing alt text") => {
     return `<picture>
     <source srcset="/img/${url}?nf_resize=fit&w=320" media="(max-width: 320px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=375" media="(min-width: 375px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=414" media="(min-width: 414px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=768" media="(min-width: 768px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=1024" media="(min-width: 1024px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=375" media="(max-width: 375px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=414" media="(max-width: 414px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=768" media="(max-width: 768px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=1024" media="(max-width: 1024px)">
     <img src="/img/${url}?nf_resize=fit&w=500" alt="${alt}" />
   </picture>`;
   };
