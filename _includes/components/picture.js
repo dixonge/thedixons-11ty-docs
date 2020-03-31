6233f2699531bdf51fcc34f267f2f73d3440dcc9
Glenn Dixon <donblanco@posteo.net>
2020-03-31 12:06:18 -0600

    fix image size options

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -1,7 +1,10 @@
 module.exports = (url, alt = "Missing alt text") => {
     return `<picture>
-    <source srcset="/img/${url}?nf_resize=fit&w=700" media="(min-width: 1200px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=600" media="(min-width: 740px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=320" media="(max-width: 320px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=375" media="(min-width: 375px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=414" media="(min-width: 414px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=768" media="(min-width: 768px)">
+    <source srcset="/img/${url}?nf_resize=fit&w=1024" media="(min-width: 1024px)">
     <img src="/img/${url}?nf_resize=fit&w=500" alt="${alt}" />
   </picture>`;
   };
