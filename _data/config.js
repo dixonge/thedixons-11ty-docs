8557232969a8bc5395f1abcc734206b91e9d3899
Glenn Dixon <donblanco@posteo.net>
2020-04-04 16:18:46 -0600

    recreated file after build fail

---
+++ _data/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	outdated: false,
+	prerelease: false,
+	minifyHtml: true,
+	githubEdit: "https://github.com/dixonge/thedixons-11ty-docs/blob/master/",
+	now: new Date(),
+	localDev: false
+};
