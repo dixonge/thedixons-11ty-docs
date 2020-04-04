995c937276ede56dea92ca42d1642356e2f250c5
Glenn Dixon <donblanco@posteo.net>
2020-04-04 08:35:47 -0600

    clean up unused files/fonts

--- _data/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	outdated: false,
-	prerelease: false,
-	minifyHtml: true,
-	githubEdit: "https://github.com/dixonge/thedixons-11ty-docs/blob/master/",
-	now: new Date(),
-	localDev: false
-};
