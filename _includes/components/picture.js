45702bd80a1d4619115abaf4c5bfd746df6d4bb3
Glenn Dixon <donblanco@posteo.net>
2020-04-05 12:24:57 -0500

    change image sizes

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -12,5 +12,5 @@ module.exports = (url, alt = "Missing alt text") => {
 */
 
 module.exports = (url, alt = "Missing alt text") => {
-    return `<img src="/img/${url}?nf_resize=fit&w=320" srcset="/img/${url}?nf_resize=fit&w=320 320w, /img/${url}?nf_resize=fit&w=480 480w, /img/${url}?nf_resize=fit&w=756 756w" sizes="(max-width: 320px) 280px, (max-width: 480px) 440px, 756px" alt="${alt}">`;
+    return `<img src="/img/${url}?nf_resize=fit&w=320" srcset="/img/${url}?nf_resize=fit&w=320 320w, /img/${url}?nf_resize=fit&w=414 414w, /img/${url}?nf_resize=fit&w=756 756w" sizes="(min-width: 320px) 290px, (min-width: 414px) 384px, 756px" alt="${alt}">`;
   };
