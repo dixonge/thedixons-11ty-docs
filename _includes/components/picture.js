751cb64e5e9adaa256450bccf4666371eaae1c8a
Glenn Dixon <donblanco@posteo.net>
2020-04-05 14:17:37 -0500

    remove src

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -12,5 +12,5 @@ module.exports = (url, alt = "Missing alt text") => {
 */
 
 module.exports = (url, alt = "Missing alt text") => {
-    return `<img src="/img/${url}?nf_resize=fit&w=320" srcset="/img/${url}?nf_resize=fit&w=320 320w, /img/${url}?nf_resize=fit&w=756 756w" sizes="(min-width: 740px) 756px, 320px" alt="${alt}">`;
+    return `<img srcset="/img/${url}?nf_resize=fit&w=320 320w, /img/${url}?nf_resize=fit&w=756 756w" sizes="(min-width: 740px) 756px, 320px" alt="${alt}">`;
   };
