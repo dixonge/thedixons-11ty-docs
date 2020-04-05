35c3530df2d2a709a88b18c46d563035c2fb34b6
Glenn Dixon <donblanco@posteo.net>
2020-04-05 14:23:43 -0500

    remove larger size, revise default

--- _includes/components/picture.js
+++ _includes/components/picture.js
@@ -4,9 +4,8 @@ module.exports = (url, alt = "Missing alt text") => {
     <source srcset="/img/${url}?nf_resize=fit&w=320" media="(max-width: 320px)">
     <source srcset="/img/${url}?nf_resize=fit&w=375" media="(max-width: 375px)">
     <source srcset="/img/${url}?nf_resize=fit&w=414" media="(max-width: 414px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=768" media="(max-width: 768px)">
-    <source srcset="/img/${url}?nf_resize=fit&w=1024" media="(max-width: 1024px)">
-    <img src="/img/${url}?nf_resize=fit&w=500" alt="${alt}" />
+    <source srcset="/img/${url}?nf_resize=fit&w=756" media="(min-width: 755px)">
+    <img src="/img/${url}?nf_resize=fit&w=756" alt="${alt}" />
   </picture>`;
   };
 /*
